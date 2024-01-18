5efaf3276c30510afa8b19fb95dc7571bfc85105
Lucas <lucaslops12@gmail.com>
2024-01-18 14:41:01 -0300

    dashboard finished

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -1,12 +1,84 @@
-const Tought = require('../models/Tought')
-const User = require('../models/User')
+const Tought = require("../models/Tought");
+const User = require("../models/User");
 
-module.exports = class ToughtController{
-    static async showToughts(req, res){
-        res.render('toughts/home');
-    }
-    static async dashboard(req, res){
-        res.render('toughts/dashboard');
+module.exports = class ToughtController {
+  static async showToughts(req, res) {
+    res.render("toughts/home");
+  }
 
+  static async dashboard(req, res) {
+    const userId = req.session.userid;
+
+    const user = await User.findOne({
+      where: {
+        id: userId,
+      },
+      include: Tought,
+      plain: true,
+    });
+
+    if (!user) {
+      res.redirect("/login");
     }
-}
+
+    const toughts = user.Toughts.map((result) => result.dataValues);
+
+    let emptyToughts = false
+
+    if(toughts.length === 0){
+        emptyToughts = true
+    }
+
+    res.render("toughts/dashboard", { toughts, emptyToughts });
+  }
+
+
+  static createTought(req, res) {
+    res.render("toughts/create");
+  }
+  static async createToughtSave(req, res) {
+    const tought = {
+      tittle: req.body.tittle,
+      UserId: req.session.userid,
+    };
+    //check if user exists
+    const userExist = await User.findByPk(tought.UserId);
+
+    if (userExist) {
+      try {
+        await Tought.create(tought);
+
+        req.flash("message", "Pensamento criado com sucesso!");
+
+        req.session.save(() => {
+          res.redirect("dashboard");
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      req.flash("message", "Tente novamente!");
+
+      req.session.save(() => {
+        res.redirect("dashboard");
+      });
+    }
+  }
+
+  static async removeTought(req, res) {
+    const id = req.body.id;
+    const UserId = req.session.userid;
+
+    try {
+      await Tought.destroy({ where: { id: id, UserId: UserId } });
+
+      req.flash("message", "Pensamento deletado com sucesso!");
+
+      req.session.save(() => {
+        res.redirect("dashboard");
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -7,6 +7,9 @@ const checkAuth = require('../helpers/auth').checkAuth
 
 
 router.get('/dashboard',checkAuth ,ToughtController.dashboard);
+router.get('/add',checkAuth ,ToughtController.createTought);
+router.post('/add',checkAuth ,ToughtController.createToughtSave);
+router.post('/remove',checkAuth ,ToughtController.removeTought);
 router.get('/', ToughtController.showToughts);
 
 module.exports = router;
